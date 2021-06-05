1f871f141d4add5e1d4022bbe92e21da7d52367b
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-06 01:51:19 +0900

    better name

--- src/components/tailwind-separate-block.js
+++ src/components/tailwind-separate-block.js
@@ -1,11 +1,11 @@
-const Small = ({ children, src, className }) => (
+const VerticalBlock = ({ children, src, className }) => (
   <div className={className}>
     <div className="pb-lg space-y-lg">{children}</div>
     <img className="block w-full" src={src} alt="" />
   </div>
 );
 
-const Large = ({ children, src, className }) => (
+const HorizontalBlock = ({ children, src, className }) => (
   <div className={`-ml-xl ${className}`}>
     <div className="flex w-full">
       <div className="pb-lg space-y-lg pl-xl w-1/2">{children}</div>
@@ -18,7 +18,7 @@ const Large = ({ children, src, className }) => (
 
 export const TailwindSeparateBlock = (props) => (
   <>
-    <Small className="lg:hidden" {...props} />
-    <Large className="hidden lg:block" {...props} />
+    <VerticalBlock className="lg:hidden" {...props} />
+    <HorizontalBlock className="hidden lg:block" {...props} />
   </>
 );
--- src/components/wysiwyg-block.js
+++ src/components/wysiwyg-block.js
@@ -23,14 +23,14 @@ const richtext = css`
   }
 `;
 
-const Small = ({ children, src, className }) => (
+const VerticalBlock = ({ children, src, className }) => (
   <div className={className}>
     <div className={`pb-lg ${richtext}`}>{children}</div>
     <img className="block w-full" src={src} alt="" />
   </div>
 );
 
-const Large = ({ children, src, className }) => (
+const HorizontalBlock = ({ children, src, className }) => (
   <div className={`-ml-xl ${className}`}>
     <div className="flex w-full">
       <div className={`pb-lg pl-xl w-1/2 ${richtext}`}>
@@ -45,7 +45,7 @@ const Large = ({ children, src, className }) => (
 
 export const WysiwygBlock = (props) => (
   <>
-    <Small className="lg:hidden" {...props} />
-    <Large className="hidden lg:block" {...props} />
+    <VerticalBlock className="lg:hidden" {...props} />
+    <HorizontalBlock className="hidden lg:block" {...props} />
   </>
 );
